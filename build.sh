0e31e29c60cb1ef8e1698068d461f04dabb34ceb
chronustorm <kishida@whill.jp>
2018-12-23 14:11:20 +0900

    Store test results.

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ clang -c add.c
 clang++ -lgtest -std=c++11 -pthread -g -Wall -Wextra -o unit_test.o -c test.cpp
 clang++ -std=c++11 -g -Wall -Wextra -o unit_test add.o unit_test.o -lgtest -pthread
 echo "Running unit tests..."
-./unit_test -v
+./unit_test -v --gtest_output=xml:test-results/tests.xml
 result=$?
 rm -r unit_test add.o unit_test.o
 echo "Unit tests completed : $result"
