509b0819cb80cffed4da4aa14bb352a7ba3235fb
seiya0412 <shimizu@whill.jp>
2019-01-01 00:58:17 +0900

    Change target source from add.c to queue.c

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 echo "Compiling unit tests..."
-clang -c add.c
+clang -c queue.c
 clang++ -lgtest -std=c++11 -pthread -g -Wall -Wextra -o unit_test.o -c test.cpp
-clang++ -std=c++11 -g -Wall -Wextra -o unit_test add.o unit_test.o -lgtest -pthread
+clang++ -std=c++11 -g -Wall -Wextra -o unit_test queue.o unit_test.o -lgtest -pthread
 echo "Running unit tests..."
 ./unit_test -v --gtest_output=xml:test-results/tests.xml
 result=$?
-rm -r unit_test add.o unit_test.o
+rm -r unit_test queue.o unit_test.o
 echo "Unit tests completed : $result"
 exit $result
